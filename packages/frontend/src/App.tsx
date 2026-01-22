6445d36fbb8c0e9875dc62d729a4d60c641ea27f
Hossam Hamdy <hossamhamdy117@gmail.com>
2026-01-22 13:36:17 +0200

    feat: Initialize new frontend application with React, Vite, and UI components.

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="p-1 text-9xl">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
